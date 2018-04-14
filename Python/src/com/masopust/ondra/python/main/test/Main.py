bae2362468e25a8fee1e9e7d429bb8d57ca79ab0
Ondřej Masopust <ondra@masopust.com>
2018-04-14 15:16:04 +0200

    edited testing Main.py to its previous form

--- Python/src/com/masopust/ondra/python/main/test/Main.py
+++ Python/src/com/masopust/ondra/python/main/test/Main.py
@@ -1,37 +1,36 @@
-#!/usr/bin/env python3.6
 '''
-Created on Aug 3, 2017
-
-This module is just to test the communication over TCP.
-
-@author: Ondrej Masopust
+ Created on Aug 3, 2017
+ 
++This module is just to test the communication over TCP.
++
+ @author: Ondrej Masopust
 '''
 import sys
-sys.path.append('/home/pi/Documents/Ondra/Rover/src/src')
-#sys.path.append('/Users/Ondra/Documents/Programming/Maturita/Project/Python/src/')
-import timeit
+sys.path.append('/Users/Ondra/Documents/Programming/Maturita/Project/Python/src/')
 
 from com.masopust.ondra.python.tcpCommunication.TCPCommunication import TCPCommunication
-
-global tcpCommunication
-
-def test():
-    tcpCommunication.sendToHost('a')
 
 def Main():
     #create server and wait for connection
     tcpCommunication = TCPCommunication()
     tcpCommunication.establishTCPConnection()
-    
+   
     #initialize sensors
     #sensors = Sensors
     #sensors.initSens()
     #tcpCommunication.sendToHost('Sensors initialized. Starting measuring.')
     #start measuring
     #sensors.start()
-    
-    print(timeit.timeit("test()", setup="from __main__ import test", number=1))
-    
+   
+    #listen to commands
+    while True:
+        data = tcpCommunication.handleRecvAndSend()
+        if data != '':
+            if data == 'stop': #'stop' returned from the tcpCommunication.handleRecvAndSend() function
+                break
+            else:
+                print(data)
+                #check for commands
     print('Client disconnected - terminating program')
     #terminate all threads
 
